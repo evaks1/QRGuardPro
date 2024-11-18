35309bcb6a854ac924724ae967c799092633c7c9
Anna Shats <ashats.ieu2022@student.ie.edu>
2024-11-18 11:47:07 +0100

    nothing

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QRGuardPro_FILE /Users/annashats/Desktop/c++/project/QRGuardPro/build/QRGuardPro)
-set(__QT_DEPLOY_TARGET_QRGuardPro_TYPE EXECUTABLE)
--- build/CMakeFiles/QRGuardPro.dir/qrc_rgguardpro.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/QRGuardPro.dir/qrc_rgguardpro.cpp.o: \
-  /Users/annashats/Desktop/c++/project/QRGuardPro/build/qrc_rgguardpro.cpp
